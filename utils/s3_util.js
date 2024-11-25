625a278744e09ea6fcc848f4db7cf5a744a3f7b0
enble <enble_777@naver.com>
2024-11-26 01:13:28 +0900

    feat: s3 upload function 추가

---
+++ utils/s3_util.js
@@ -0,0 +1,40 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+
+const {
+  AWS_S3_REGION,
+  AWS_S3_BUCKET_NAME,
+  AWS_S3_ACCESS_KEY,
+  AWS_S3_SECRET_KEY,
+} = require("../config/config");
+
+aws.config.update({
+  region: AWS_S3_REGION,
+  accessKeyId: AWS_S3_ACCESS_KEY,
+  secretAccessKey: AWS_S3_SECRET_KEY,
+});
+
+const s3 = new aws.S3();
+const allowedExt = [".jpg", ".jpeg", ".png", ".bmp"];
+
+const uploadImage = multer({
+  storage: multerS3({
+    s3: new aws.S3(),
+    bucket: AWS_S3_BUCKET_NAME,
+    key: (req, file, cb) => {
+      const extension = path.extname(file.originalname);
+      if (!allowedExt.includes(extension)) {
+        return cb(new Error("Only jpg, jpeg, png, bmp are allowed"));
+      }
+      const key = `images/${Date.now()}_${file.originalname}`;
+      const imageUrl = `https://${AWS_S3_BUCKET_NAME}.s3.${AWS_S3_REGION}.amazonaws.com/${key}`;
+      file.location = imageUrl; // Add image URL to file object
+      cb(null, key);
+    },
+    acl: "public-read-write",
+  }),
+});
+
+module.exports = uploadImage;
